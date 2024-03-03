ddbb8dcdfaee4ac4790b3983788db7ed1bc7129d
Jp Ludin <>
2024-03-03 11:15:15 +0100

    TASK/Add more choices to the game

--- vite-project/Quiz/src/App.jsx
+++ vite-project/Quiz/src/App.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import StartPage from './StartPage'
 import QuestionPage from './QuestionPage'
 
-export default function App() {
-  const [gameStarted, setGameStarted] = React.useState(false)
+export default function App(props) {
+  const [gameStarted, setGameStarted] = React.useState(false);
+  const [questionAmount, setQuestionAmount] = React.useState(5);
+  const [questionDifficulty, setQuestionDifficulty] = React.useState('easy');
 
-
-  function startGame() {
-
+  function startGame(selectedAmount, selectedDifficulty) {
     setGameStarted(true)
+    setQuestionAmount(selectedAmount)
+    setQuestionDifficulty(selectedDifficulty)
   }
   return (
     <main>
       {gameStarted ?
-        <QuestionPage questionAmount={amountToFetch}/> 
+        <QuestionPage questionAmount={questionAmount} questionDifficulty={questionDifficulty}/> 
         : <StartPage handleClick={startGame}/>
       }
     </main>
--- vite-project/Quiz/src/StartPage.jsx
+++ vite-project/Quiz/src/StartPage.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import './Startpage.css'
+import React from "react"
+import QuestionAmount from "./QuestionAmount"
+import QuestionDifficulty from "./QuestionDifficulty"
+import './StartPage.css'
 
 export default function StartPage(props) {
+    const [selectedAmount, setSelectedAmount] = React.useState(5)
+    const [selectedDifficulty, setselectedDifficulty] = React.useState('easy')
+    
+    const handleNumberOfQuestionsChange = (event) => {
+        setSelectedAmount(Number(event.target.value))
+    }
+
+    const handleDifficultyChange  = (event) => {
+        setselectedDifficulty(event.target.value)
+    }
+
+    function handleClick() {
+        props.handleClick(selectedAmount, selectedDifficulty)
+    }
     return (
-        <div>
-            <h1 className="title">Quizzical</h1>
-            <p className="text">This is a quiz game</p>
-            <a href="#" className="start-btn" onClick={props.handleClick}>Start quiz</a>
+        <div className="start-container">
+            <h1 className="title">Welcome to QuizMaster!</h1>
+            <p className="text">Dive into the world of QuizMaster, where your knowledge will be tested, and your quick thinking rewarded! Whether you're a trivia enthusiast or just looking for a fun way to pass the time, you've come to the right place. Prepare to challenge yourself across various categories, including science, history, pop culture, and more. Are you ready to claim the title of QuizMaster? Let's get started!</p>
+            <QuestionAmount 
+                questionAmount={selectedAmount} 
+                handleNumberOfQuestionsChange={handleNumberOfQuestionsChange} /> 
+            <QuestionDifficulty 
+                questionDifficulty={selectedDifficulty} 
+                handleDifficultyChange={handleDifficultyChange} />          
+            <a href="#" className="start-btn" onClick={handleClick}>Start quiz</a>
         </div>
     )
 }
